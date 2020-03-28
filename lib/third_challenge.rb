dd7e000f9989bcafb2c01041c914c477612dd120
chris brady <chrisb3546@yahoo.com>
2020-03-28 17:29:19 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,7 +10,7 @@ def third_challenge
       :hero => { :name => "Romeo"
         #your key/value pairs here
         },
-      :hero_friends => []
+      :hero_friends => [{:name => }]
    }, 
    :capulet => {
       :patriarch => {
