b6c2fcb8cfc4045951af852ce8dd57854835d7d2
chris brady <chrisb3546@yahoo.com>
2020-03-28 17:16:48 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,17 +2,18 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
-      :patriarch =>{
-      :matriarch =>{
-      :hero =>{
-      :hero_friends =>{ 
+      :patriarch =>{ }
+      :matriarch =>{ }
+      :hero =>{ }
+      :hero_friends =>{ }
       }
-    :capulet => { }
-      :patriarch =>{
-      :matriarch =>{
-      :hero =>
-      :hero_friends =>{
+    :capulet => { 
+      :patriarch =>{ }
+      :matriarch =>{ }
+      :hero => { }
+      :hero_friends =>{ }
       }
+    }
 end
   
 end
