038d13ea904cf2f7123516e392458024885b0ebd
chris brady <chrisb3546@yahoo.com>
2020-03-28 17:05:24 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,9 +7,12 @@ def second_challenge
       :hero, 
       :hero_friends
       , 
-  }
+  }}
     :capulet => {
-      #your key/value pairs here!
+      :patriarch,
+      :matriarch,
+      :hero, 
+      :hero_friends
       }}
 end
   
