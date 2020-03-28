5503cea93b5dc93e43a5764a213842a6175d935c
chris brady <chrisb3546@yahoo.com>
2020-03-28 17:40:23 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -30,9 +30,9 @@ def third_challenge
         },
       :heroine => {
         #your key/value pairs here
-        :name => "Juliet"
+        :name => "Juliet",
         :age => "15",
-        :status => "alive."
+        :status => "alive"
         },
       :heroine_friends => []
    }
